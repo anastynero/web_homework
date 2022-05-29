5343128e5efaddabffbcc44467533b676ce26eea
anastynero <90865165+anastynero@users.noreply.github.com>
2022-05-29 11:14:11 +0300

    Update 6.js

--- lab12/6.js
+++ lab12/6.js
@@ -1,8 +1,8 @@
-let poly = 2002
-var ylop = 0;
-var number = poly;
-var isPalindrome = false;
-for(; poly; poly = Math.floor(poly / 10)) {
+let poly = prompt();
+let ylop = 0;
+let number = poly;
+let isPalindrome = false;
+for(;poly; poly = Math.floor(poly / 10)) {
     ylop = ylop * 10 + poly % 10;
 }
 if(ylop === number) {
